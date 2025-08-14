e997053912e7135bbef5562b0d28c2c2476d448c
Coder-Aman-868 <developeraman868@gmail.com>
2025-08-14 13:49:00 +0530

    frontend setup ready

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import SinglePost from "./pages/SinglePost";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+          <Route path="/post/:id" element={<SinglePost />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
